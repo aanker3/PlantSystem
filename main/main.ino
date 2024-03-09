500c0db0af842a80bd347b56fe66aa8729c86a28
alex anker <alanker112@gmail.com>
2024-03-09 14:48:13 -0800

    clean

--- main/main.ino
+++ main/main.ino
@@ -6,8 +6,6 @@
 #include "src/objects/hw_peripherals/motor.h"
 #include <vector>
 
-#define START_OF_TIMER 0
-
 WateringManager* wateringManager = NULL;
 
 // Global Variables
@@ -27,12 +25,12 @@ void setup() {
 
 void loop() {
 
-  // Call GetTimeAndUpdate regularly to update time and count days
+  // Call GetTimeAndUpdate to update time and count days
   getTimeAndUpdate(previousTime, daysElapsed);
 
   // Check if two weeks have elapsed.
   if (twoWeeksElapsed(daysElapsed)) {
-    weeksElapsed++;
+    weeksElapsed = weeksElapsed + 2;
     //Reset watering period
     wateringManager->printTwoWeekResults(weeksElapsed);
     wateringManager->resetPlants();
@@ -42,55 +40,9 @@ void loop() {
     Serial.println("days elapsed = " + String(daysElapsed));
   }
 
-  Serial.println("test");
   //wateringManager->printPlantSystemsInfo();
   wateringManager->gatherSensorData();
   wateringManager->waterPlantsIfNeeded();
 
   delay(1000);
 }
-
-
-//   void loop() {
-//   Serial.println("Cactus Moisture Value = " + String(cactus->getMoistureWateringPoint()));
-//   moisture.getMeasuredValue();
-//   float moisture_value = moisture.getMeasuredValue();
-
-//   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-//   Serial.println("Moisture_Sensor_A0: " + String(moisture_value));
-
-//   GetTimeAndUpdate(previousTime, daysElapsed);
-//   Serial.println("daysElapsed = " + String(daysElapsed));
-
-
-
-//   Serial.println("TimeElapsed= " + String(GetTimeElapsedMinutes()) + " Minutes");
-  
-//   if(moisture_value < fern->getMoistureWateringPoint())
-//   {
-//     Serial.println("WATERING!!! SENSOR_VALUE = " + String(moisture_value));
-//     fernMotor.motor5s();
-//     fern->plantWatered();
-//     Serial.println("End Watering.  Times Watered = " + String(fern->getTimesWatered()));
-//   }
-//   delay(1000);  // Delay a bit to make it more readable
-// }
-
-
-//   void loop() {
-//   float sensor_valueA0 = SensorValueInVoltage(analogRead(A0));
-
-//   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-//   Serial.print("Moisture_Sensor_A0: " + String(sensor_valueA0));
-
-//   if(sensor_valueA0 < fern.getCurrentMoistureValue())
-//   {
-//     Serial.print("WATERING!!! SENSOR_VALUE = " + String(sensor_valueA0));
-
-//     digitalWrite(motorPin, HIGH); // Turn the motor on
-//     delay(5000);
-//     digitalWrite(motorPin, LOW);  // Turn the motor of
-//     Serial.print("End Watering");
-//   }
-//   delay(1000);  // Delay a bit to make it more readable
-// }
--- main/src/objects/plants/plant_factory.cpp
+++ main/src/objects/plants/plant_factory.cpp
@@ -6,7 +6,6 @@ Plant* PlantFactory::createPlant(const std::string& plantName, PotSize potSize)
     if (it != plantConfigurations.end()) {
         return new Plant(plantName, potSize, it->second);
     } else {
-        //add a print here for err  Maybe notify user a different way.
         return NULL;
     }
 }
